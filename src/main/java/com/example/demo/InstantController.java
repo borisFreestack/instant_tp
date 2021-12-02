769a2a422eb1132e8b4652284b079cc021e899e2
Boris Smets <boris.smets@freestack.rocks>
2021-12-02 09:05:23 +0100

    init

---
+++ src/main/java/com/example/demo/InstantController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class InstantController {
+
+
+}
